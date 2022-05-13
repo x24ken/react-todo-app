7dc9d1d40d5d0caaa6a7444c1147f0789347edd3
x24ken <x24ken@gmail.com>
2022-05-13 14:22:23 +0900

    こんぽーねんとか

---
+++ src/compornents/IncompleteArea.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const IncompleteArea = (props) => {
+  const { inCompleteTodos, onClickComplete, onClickDelete } = props;
+  return (
+    <div className="incomplete-area">
+      <p className="title">未完了のTODO</p>
+      <ul>
+        {inCompleteTodos.map((todo, index) => {
+          return (
+            <div key={todo} className="list-row">
+              <li>{todo}</li>
+              <button onClick={() => onClickComplete(index)}>完了</button>
+              <button onClick={() => onClickDelete(index)}>削除</button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
